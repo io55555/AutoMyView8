cfa74d45e4ee2e081b2a6f65331c37441de26de6
@ <@>
2026-04-21 00:45:08 +0800

    *

--- scripts/v8dasm-builders/build-windows.cmd
+++ scripts/v8dasm-builders/build-windows.cmd
@@ -224,9 +224,11 @@ call :log_line "Patch helper summary: PATCH_STATUS=%PATCH_STATUS_VALUE%"
 
 if not exist "%DASM_SOURCE%" call :fail "CLANG" "Source file not found: %DASM_SOURCE%"
 
-set GN_ARGS=target_os=\"win\" target_cpu=\"x64\" is_component_build=false is_debug=false use_custom_libcxx=false v8_monolithic=true v8_static_library=true v8_enable_disassembler=true v8_enable_object_print=true v8_use_external_startup_data=false dcheck_always_on=false symbol_level=0 is_clang=true extra_cflags_cc=[\"/D_SILENCE_CXX20_OLD_SHARED_PTR_ATOMIC_SUPPORT_DEPRECATION_WARNING\"]
+set GN_ARGS=target_os=\"win\" target_cpu=\"x64\" is_component_build=false is_debug=false use_custom_libcxx=false v8_monolithic=true v8_static_library=true v8_enable_disassembler=true v8_enable_object_print=true v8_use_external_startup_data=false dcheck_always_on=false symbol_level=0 is_clang=true
 if not "%BUILD_ARGS%"=="" set GN_ARGS=%GN_ARGS% %BUILD_ARGS%
+set CL=/D_SILENCE_CXX20_OLD_SHARED_PTR_ATOMIC_SUPPORT_DEPRECATION_WARNING %CL%
 call :log_line "GN Args: %GN_ARGS%"
+call :log_line "CL env: %CL%"
 
 call :stage "GN"
 pushd "%V8_DIR%" >nul 2>&1 || call :fail "GN" "Failed to enter %V8_DIR%"
